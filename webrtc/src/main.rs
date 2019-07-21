e8145be06599d22775f76eb860bd0a8b77d9aed4
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2019-07-21 20:58:27 +0100

    Get SDP working with Chromium RTCPeerConnection
    This uses Chromium and the gstreamer-send fiddle from
    https://github.com/pion/example-webrtc-applications
    to test that the generated SDP is acceptable and that the browser is
    happy to attempt a connection to the gathered candidates.
    
    It needs a lot of cleaning up, mainly around the networking, due to the
    threading and the way the JoinHandles are passed around, but it's a
    start!

--- webrtc/src/main.rs
+++ webrtc/src/main.rs
@@ -1,5 +1,5 @@
 use std::net::{IpAddr, SocketAddr, UdpSocket};
-use std::thread;
+use std::thread::{self, JoinHandle};
 
 use env_logger;
 use failure::Error;
@@ -22,7 +22,7 @@ fn rand_ice_string(length: usize) -> String {
     String::from_utf8(random_chars).unwrap()
 }
 
-fn udp_listener(address: IpAddr) -> Result<SocketAddr, Error> {
+fn udp_listener(address: IpAddr) -> Result<(SocketAddr, JoinHandle<()>), Error> {
     debug!("Starting UDP listener on {}", address);
 
     let socket = UdpSocket::bind(format!("{}:0", address))?;
@@ -33,19 +33,38 @@ fn udp_listener(address: IpAddr) -> Result<SocketAddr, Error> {
         local_addr.port()
     );
 
-    thread::spawn(move || {
+    let handle = thread::spawn(move || {
         let mut buf = [0; MTU];
-        // TODO: remove unwrap
+        trace!("Receiving on {:?}", socket.local_addr());
         let (bytes_rcvd, src_addr) = socket.recv_from(&mut buf).unwrap();
         trace!("Received {} bytes from {}", bytes_rcvd, src_addr);
     });
 
-    Ok(local_addr)
+    Ok((local_addr, handle))
 }
 
-fn main() {
-    env_logger::init();
+fn encode_as_sdp(foundation: usize, candidate: SocketAddr) -> sdp::Attribute {
+    let component_id = 1; // RTP == 1
 
+    let transport = "udp";
+
+    let ip_precedence = 65535; // IPv4 only
+    let priority = ((2_i64.pow(24)) * 126) + ((2_i64.pow(8)) * ip_precedence) + 256 - component_id;
+
+    let v = format!(
+        "{} {} {} {} {} {} typ host",
+        foundation,
+        component_id,
+        transport,
+        priority,
+        candidate.ip(),
+        candidate.port(),
+    );
+    sdp::Attribute::value("candidate", &v)
+}
+
+// TODO: create Candidate struct and return that instead of String
+fn gather_candidates() -> (Vec<sdp::Attribute>, Vec<JoinHandle<()>>) {
     let interfaces: Vec<_> = datalink::interfaces()
         .into_iter()
         .filter(|i| i.is_up() && !i.is_loopback())
@@ -55,16 +74,34 @@ fn main() {
         .collect();
 
     let mut local_candidates = vec![];
+    let mut join_handles = vec![];
     for interface in interfaces {
         match udp_listener(interface) {
-            Ok(candidate) => local_candidates.push(candidate),
+            Ok((candidate, handle)) => {
+                local_candidates.push(candidate);
+                join_handles.push(handle);
+            }
             Err(_) => continue,
         }
     }
-    debug!("Local candidates: {:?}", local_candidates);
+
+    let attributes = local_candidates
+        .into_iter()
+        .enumerate()
+        .map(|(i, v)| encode_as_sdp(i, v))
+        .collect();
+
+    (attributes, join_handles)
+}
+
+fn main() {
+    env_logger::init();
 
     let ice_pwd = rand_ice_string(22);
     let ice_ufrag = rand_ice_string(4);
+
+    let (mut candidates, join_handles) = gather_candidates();
+    debug!("Local candidates: {:?}", candidates);
 
     let video_description = sdp::MediaDescription::base(sdp::Media {
         typ: sdp::MediaType::Video,
@@ -77,29 +114,33 @@ fn main() {
         address_type: "IP4".to_owned(),
         connection_address: "127.0.0.1".to_owned(),
     })
-    .with_attributes(vec![
-        sdp::Attribute::value("rtpmap", "96 VP8/90000"),
-        sdp::Attribute::value("rtpmap", "97 rtx/90000"),
-        sdp::Attribute::value("fmtp", "97 apt=96"),
-        sdp::Attribute::value("ftcp-fb", "96 goog-remb"),
-        sdp::Attribute::value("ftcp-fb", "96 ccm fir"),
-        sdp::Attribute::value("ftcp-fb", "96 nack"),
-        sdp::Attribute::value("ftcp-fb", "96 nack pli"),
-        sdp::Attribute::value("extmap", "2 urn:ietf:params:rtp-hdrext:toffset"),
-        sdp::Attribute::value(
-            "extmap",
-            "3 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time",
-        ),
-        sdp::Attribute::value("extmap", "4 urn:3gpp:video-orientation"),
-        sdp::Attribute::value("setup", "active"),
-        sdp::Attribute::value("mid", "video"),
-        sdp::Attribute::property("recvonly"),
-        sdp::Attribute::value("ice-ufrag", &ice_ufrag),
-        sdp::Attribute::value("ice-pwd", &ice_pwd),
-        sdp::Attribute::value("ice-options", "renomination"),
-        sdp::Attribute::property("rtcp-mux"),
-        sdp::Attribute::property("rtcp-rsize"),
-    ]);
+    .with_attributes({
+        candidates.append(&mut vec![
+            sdp::Attribute::value("rtpmap", "96 VP8/90000"),
+            sdp::Attribute::value("rtpmap", "97 rtx/90000"),
+            sdp::Attribute::value("fmtp", "97 apt=96"),
+            sdp::Attribute::value("ftcp-fb", "96 goog-remb"),
+            sdp::Attribute::value("ftcp-fb", "96 ccm fir"),
+            sdp::Attribute::value("ftcp-fb", "96 nack"),
+            sdp::Attribute::value("ftcp-fb", "96 nack pli"),
+            sdp::Attribute::value("extmap", "2 urn:ietf:params:rtp-hdrext:toffset"),
+            sdp::Attribute::value(
+                "extmap",
+                "3 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time",
+            ),
+            sdp::Attribute::value("extmap", "4 urn:3gpp:video-orientation"),
+            sdp::Attribute::value("setup", "active"),
+            sdp::Attribute::value("mid", "0"),
+            sdp::Attribute::property("sendonly"),
+            sdp::Attribute::value("ice-ufrag", &ice_ufrag),
+            sdp::Attribute::value("ice-pwd", &ice_pwd),
+            sdp::Attribute::value("ice-options", "renomination"),
+            sdp::Attribute::property("rtcp-mux"),
+            sdp::Attribute::property("rtcp-rsize"),
+        ]);
+
+        candidates
+    });
 
     let session_description = sdp::SessionDescription::base(
         sdp::Version(0),
@@ -123,9 +164,13 @@ fn main() {
             sdp::Attribute::property("ice-lite"),
             sdp::Attribute::value("fingerprint", "sha-512 4E:DD:25:41:95:51:85:B6:6A:29:42:FF:56:5B:41:47:2C:6C:67:36:7D:97:91:5A:65:C7:E1:76:1B:6E:D3:22:45:B4:9F:DF:EA:93:FF:20:F4:CB:A8:53:AF:50:DA:87:5A:C5:4C:5B:F6:4C:50:DC:D9:29:A3:C0:19:7A:17:48"),
             sdp::Attribute::value("msid-semantic", " WMS *"),
-            sdp::Attribute::value("group", "BUNDLE video"),
+            sdp::Attribute::value("group", "BUNDLE 0"),
         ],
     ).and_media_description(video_description);
 
-    debug!("{:?}", session_description);
+    print!("{}", session_description);
+
+    for handle in join_handles {
+        handle.join().unwrap();
+    }
 }
