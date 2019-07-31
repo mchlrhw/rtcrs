6f1f0a40077bc944c33cda84f74f84895e0996fd
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2019-07-31 21:00:32 +0100

    Move TLV parsing into priority

--- stun/src/attribute/priority.rs
+++ stun/src/attribute/priority.rs
@@ -1,15 +1,19 @@
 use std::convert::TryInto;
 
-use nom::IResult;
+use nom::{
+    bytes::complete::tag, multi::length_data, number::complete::be_u16, sequence::preceded, IResult,
+};
 
 use crate::attribute::{Attribute, Tlv};
+
+const TYPE: u16 = 0x_0024;
 
 #[derive(Debug, PartialEq)]
 pub struct Priority(u32);
 
 impl Tlv for Priority {
     fn typ(&self) -> u16 {
-        0x_0024
+        TYPE
     }
 
     fn length(&self) -> u16 {
@@ -22,12 +26,33 @@ impl Tlv for Priority {
 }
 
 pub(crate) fn priority(input: &[u8]) -> IResult<&[u8], Attribute> {
-    let (input, remainder) = input.split_at(4);
-    let input: [u8; 4] = input.try_into().unwrap();
-    let value = u32::from_be_bytes(input);
+    let (remainder, value_field) = preceded(tag(TYPE.to_be_bytes()), length_data(be_u16))(input)?;
+
+    // TODO: return Err here
+    let value_field: [u8; 4] = value_field.try_into().unwrap();
+    let value = u32::from_be_bytes(value_field);
 
     let inner = Priority(value);
     let attribute = Attribute::Priority(inner);
 
     Ok((remainder, attribute))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn round_trip_bytes() {
+        #[rustfmt::skip]
+        let input = [
+            0x_00, 0x_24, 0x_00, 0x_04,
+            0x_DE, 0x_AD, 0x_BE, 0x_EF,
+        ];
+
+        let (_, attribute) = priority(&input).unwrap();
+        let attribute_bytes = attribute.to_bytes();
+
+        assert_eq!(attribute_bytes, input);
+    }
 }
