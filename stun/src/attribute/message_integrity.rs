a6a980bd7f57875021c37670aa04100b0169b47f
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2019-07-31 20:55:00 +0100

    Move TLV parsing into message integrity

--- stun/src/attribute/message_integrity.rs
+++ stun/src/attribute/message_integrity.rs
@@ -1,8 +1,12 @@
 use std::convert::TryInto;
 
-use nom::IResult;
+use nom::{
+    bytes::complete::tag, multi::length_data, number::complete::be_u16, sequence::preceded, IResult,
+};
 
 use crate::attribute::{Attribute, Tlv};
+
+const TYPE: u16 = 0x_0008;
 
 #[derive(Debug, PartialEq)]
 pub struct MessageIntegrity(Vec<u8>);
@@ -15,7 +19,7 @@ impl MessageIntegrity {
 
 impl Tlv for MessageIntegrity {
     fn typ(&self) -> u16 {
-        0x_0008
+        TYPE
     }
 
     fn length(&self) -> u16 {
@@ -28,11 +32,36 @@ impl Tlv for MessageIntegrity {
 }
 
 pub(crate) fn message_integrity(input: &[u8]) -> IResult<&[u8], Attribute> {
-    let (input, remainder) = input.split_at(20);
-    let value = input.to_vec();
+    let (remainder, value_field) = preceded(tag(TYPE.to_be_bytes()), length_data(be_u16))(input)?;
+
+    // TODO: assert that value_field.len() == 20
+    let value = value_field.to_vec();
 
     let inner = MessageIntegrity(value);
     let attribute = Attribute::MessageIntegrity(inner);
 
     Ok((remainder, attribute))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn round_trip_bytes() {
+        #[rustfmt::skip]
+        let input = [
+            0x_00, 0x_08, 0x_00, 0x_14,
+            0x_DE, 0x_AD, 0x_BE, 0x_EF,
+            0x_CA, 0x_FE, 0x_BA, 0x_BE,
+            0x_CA, 0x_FE, 0x_D0, 0x_0D,
+            0x_FE, 0x_E1, 0x_DE, 0x_AD,
+            0x_FE, 0x_ED, 0x_FA, 0x_CE,
+        ];
+
+        let (_, attribute) = message_integrity(&input).unwrap();
+        let attribute_bytes = attribute.to_bytes();
+
+        assert_eq!(attribute_bytes, input);
+    }
 }
